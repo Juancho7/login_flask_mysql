edb0eae85b9d8d7e1e36a43fde5af6ac3fa88faa
juancho7 <juanfertrom@gmail.com>
2023-10-15 18:30:52 -0500

    first commit, the app was initialized

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+
+app = Flask(__name__)
+
+app.secret_key = 'your secret key'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'flask_basics'
+
+mysql = MySQL(app)
+
+
