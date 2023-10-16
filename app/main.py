d9fa7ee16f4c11e6a37a5b2af3a3a5f832a1aacb
juancho7 <juanfertrom@gmail.com>
2023-10-15 19:06:16 -0500

    login is working

--- app/main.py
+++ app/main.py
@@ -1,16 +1,47 @@
-from flask import Flask
+from flask import Flask, request, session, render_template, redirect, url_for
 from flask_mysqldb import MySQL
-import MySQLdb.cursors
+from MySQLdb.cursors import DictCursor
 
 app = Flask(__name__)
 
-app.secret_key = 'your secret key'
+app.secret_key = "your secret key"
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'flask_basics'
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "root"
+app.config["MYSQL_DB"] = "flask_basics"
 
 mysql = MySQL(app)
 
 
+@app.route("/")
+def empty():
+    return redirect(url_for("login"))
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    msg = ""
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        try:
+            cursor = mysql.connection.cursor(DictCursor)
+            sql = "SELECT * FROM login WHERE username = %s AND password = %s"
+            cursor.execute(sql, (username, password))
+            account = cursor.fetchone()
+            if account:
+                session["loggedin"] = True
+                session["id"] = account["id"]
+                session["username"] = account["username"]
+                msg = "Logged in succesfully"
+                return render_template("index.html", message=msg)
+            else:
+                msg = "Incorrect user / password"
+        except Exception as e:
+            raise e
+    return render_template("login.html", message=msg)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
